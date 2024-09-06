2e36e88cafd67aee29124b28e3a010bf192c610b
Jordan Pierce <115024024+Jordan-Pierce@users.noreply.github.com>
2024-09-06 10:07:56 -0400

    potree

---
+++ Potree/D3/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "points": 7434379,
+    "boundingBox": {
+        "lx": -12.569877,
+        "ly": -9.527239,
+        "lz": -16.545506,
+        "ux": 13.986385,
+        "uy": 17.029023,
+        "uz": 10.010757
+    },
+    "tightBoundingBox": {
+        "lx": -10.380111,
+        "ly": -7.944356,
+        "lz": -14.880739,
+        "ux": 11.367123,
+        "uy": 7.880425,
+        "uz": 1.732158
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 0.207471,
+    "scale": 0.001000,
+    "hierarchyStepSize": 5
+}
